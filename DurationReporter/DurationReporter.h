f5342f3b31c5e999ff497ff7c40e1f68c3ccc3ac
ktustanowski <kamil.tustanowski@gmail.com>
2017-05-24 14:09:34 +0200

    Initial Commit

---
+++ DurationReporter/DurationReporter.h
@@ -0,0 +1,19 @@
+//
+//  DurationReporter.h
+//  DurationReporter
+//
+//  Created by Kamil Tustanowski on 24.05.2017.
+//  Copyright © 2017 ktustanowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DurationReporter.
+FOUNDATION_EXPORT double DurationReporterVersionNumber;
+
+//! Project version string for DurationReporter.
+FOUNDATION_EXPORT const unsigned char DurationReporterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DurationReporter/PublicHeader.h>
+
+
