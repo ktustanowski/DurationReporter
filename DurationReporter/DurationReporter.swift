8f4faafdf536089b23a01eec9f336771c258f720
Kamil Tustanowski <kamil.tustanowski@gmail.com>
2017-06-17 15:14:57 +0200

    Updated licenses strings in source code.

--- DurationReporter/DurationReporter.swift
+++ DurationReporter/DurationReporter.swift
@@ -19,12 +19,6 @@
 //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.//
-//  DispatchQueue+Once.swift
-//  SwiftyOnce
-//
-//  Created by Kamil Tustanowski on 20.01.2017.
-//  Copyright © 2017 ktustanowski. All rights reserved.
-//
 
 import Foundation
 
